8434e7dc6bc8865136ed1e4cebf2e33ef953d781
Karlo M <karlo.marinovic@decode.agency>
2024-01-17 21:47:06 +0100

    pass test find date time ui

--- lib/src/features/matches/presentation/widgets/match_brief_extended.dart
+++ lib/src/features/matches/presentation/widgets/match_brief_extended.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
 
 class MatchBriefExtended extends StatelessWidget {
-  const MatchBriefExtended({super.key});
+  const MatchBriefExtended({
+    super.key,
+    required this.date,
+    required this.dayName,
+    required this.time,
+  });
+
+  final String date;
+  final String dayName;
+  final String time;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        child: Row(
+      children: [
+        Column(
+          children: [
+            Container(
+              color: Colors.blue,
+              child: Column(
+                children: [
+                  Text(
+                    date,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    dayName,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              time,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        )
+      ],
+    ));
   }
 }
