d4f03e4c309c130fac2f4a78fcc1f7bd343fc08e
Karlo M <karlo.marinovic@decode.agency>
2024-01-17 21:48:27 +0100

    pass test find date time ui

--- lib/src/features/matches/presentation/widgets/match_brief_extended.dart
+++ lib/src/features/matches/presentation/widgets/match_brief_extended.dart
@@ -17,36 +17,57 @@ class MatchBriefExtended extends StatelessWidget {
     return Container(
         child: Row(
       children: [
-        Column(
-          children: [
-            Container(
-              color: Colors.blue,
-              child: Column(
-                children: [
-                  Text(
-                    date,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    dayName,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Text(
-              time,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        )
+        _DateElement(
+          date: date,
+          dayName: dayName,
+          time: time,
+        ),
       ],
     ));
+  }
+}
+
+class _DateElement extends StatelessWidget {
+  const _DateElement({
+    required this.date,
+    required this.dayName,
+    required this.time,
+  });
+
+  final String date;
+  final String dayName;
+  final String time;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          color: Colors.blue,
+          child: Column(
+            children: [
+              Text(
+                date,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                dayName,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Text(
+          time,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
   }
 }
