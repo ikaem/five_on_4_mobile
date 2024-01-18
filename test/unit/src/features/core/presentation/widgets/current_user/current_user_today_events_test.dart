7e16b08db597334c1adec9a8f3ff690c6e664530
Karlo M <karlo.marinovic@decode.agency>
2024-01-18 18:37:19 +0100

    fail test show no matches today

---
+++ test/unit/src/features/core/presentation/widgets/current_user/current_user_today_events_test.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group(
+    "CurrentUserTodayEvents",
+    () {
+      testWidgets(
+        "given empty list of todays matches are provided"
+        "when widget is rendered"
+        "should show expexted NO MATCHES text",
+        (widgetTester) async {
+          final todaysMatches = <MatchModel>[];
+
+          await widgetTester.pumpWidget(
+            const MaterialApp(
+              home: CurrentUserTodayEvents(
+                todaysMatches: todaysMatches,
+              ),
+            ),
+          );
+
+          // find texts with messages for no match
+
+          final noMatchesText = find.text("No matches today");
+          final restTodayText = find.text("Have a rest, you deserve it!");
+        },
+      );
+    },
+  );
+}
