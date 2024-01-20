e99a35f4636c2672dc340c855fbd6ece864b96a2
Karlo M <karlo.marinovic@decode.agency>
2024-01-20 09:09:49 +0100

    fail show following events indicator

--- test/unit/src/features/core/presentation/screens/home_screen_test.dart
+++ test/unit/src/features/core/presentation/screens/home_screen_test.dart
@@ -11,6 +11,7 @@ void main() {
       group(
         "Toggle 'when' on events selector",
         () {
+          const selectorIndicator = "•";
           testWidgets(
             "given user has performed no actions"
             "when screen is rendered"
@@ -40,11 +41,50 @@ void main() {
               // );
 
               // assert style and that it finds widget
-              final todaySelector = find.text("Today •");
-              final followingEventsSelector = find.text("Following matches •");
+              final todaySelector = find.text("Today $selectorIndicator");
+              final followingEventsSelector =
+                  find.text("Following matches $selectorIndicator");
 
               expect(todaySelector, findsOneWidget);
               expect(followingEventsSelector, findsNothing);
+            },
+          );
+
+          testWidgets(
+            "given user selects 'following events'"
+            "when screen is rendered"
+            "should have 'Following' selector selected, and not 'Today'",
+            (widgetTester) async {
+              await widgetTester.pumpWidget(
+                const MaterialApp(
+                  home: HomeScreen(),
+                ),
+              );
+              // TODO maybe byWidgetPredicate can be used
+              // final todaySelector = find.byWidget(
+              //   const Text(
+              //     "Today •",
+              //     // style: TextStyle(fontWeight: FontWeight.bold),
+              //   ),
+              // );
+
+              /* TODO this can be used as well */
+              // final todaySelector = find.byWidgetPredicate(
+              //   (widget) {
+              //     if (widget is! Text) return false;
+              //     if (widget.data != "Today •") return false;
+
+              //     return true;
+              //   },
+              // );
+
+              // assert style and that it finds widget
+              final todaySelector = find.text("Today $selectorIndicator");
+              final followingEventsSelector =
+                  find.text("Following matches $selectorIndicator");
+
+              expect(todaySelector, findsNothing);
+              expect(followingEventsSelector, findsOneWidget);
             },
           );
 
