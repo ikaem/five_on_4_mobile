8b9cc741483649407462cf77623959d62ac1ba15
Karlo M <karlo.marinovic@decode.agency>
2024-01-20 11:39:20 +0100

    match info tests pass

---
+++ lib/src/features/matches/presentation/widgets/match/match_info.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+class MatchInfo extends StatelessWidget {
+  const MatchInfo({
+    super.key,
+    required this.date,
+    required this.dayName,
+    required this.time,
+    required this.title,
+    required this.location,
+    required this.organizer,
+    required this.arrivingPlayers,
+  });
+
+  final String date;
+  final String dayName;
+  final String time;
+  final String title;
+  final String location;
+  final String organizer;
+  final int arrivingPlayers;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Row(
+      children: [
+        Expanded(
+          child: _DateElement(
+            date: date,
+            dayName: dayName,
+            time: time,
+          ),
+        ),
+        Expanded(
+          child: Column(
+            children: [
+              Text(
+                title,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                location,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text.rich(
+                TextSpan(
+                  children: [
+                    const TextSpan(
+                      text: "Organized by: ",
+                    ),
+                    TextSpan(
+                      text: organizer,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Text.rich(
+                TextSpan(
+                  children: [
+                    const TextSpan(
+                      text: "Arriving players: ",
+                    ),
+                    TextSpan(
+                      text: arrivingPlayers.toString(),
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    ));
+  }
+}
+
+class _DateElement extends StatelessWidget {
+  const _DateElement({
+    required this.date,
+    required this.dayName,
+    required this.time,
+  });
+
+  final String date;
+  final String dayName;
+  final String time;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          color: Colors.blue,
+          child: Column(
+            children: [
+              Text(
+                date,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                dayName,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Text(
+          time,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+  }
+}
--- test/unit/src/features/matches/presentation/widgets/match/match_info_test.dart
+++ test/unit/src/features/matches/presentation/widgets/match/match_info_test.dart
@@ -1,3 +1,4 @@
+import 'package:five_on_4_mobile/src/features/matches/presentation/widgets/match/match_info.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -34,6 +35,115 @@ void main() {
           expect(dateText, findsOneWidget);
           expect(dayNameText, findsOneWidget);
           expect(timeText, findsOneWidget);
+        },
+      );
+
+      testWidgets(
+        "given title argument is provided"
+        "when widget is rendered"
+        "should show expected title",
+        (widgetTester) async {
+          const title = "testTitle";
+
+          await widgetTester.pumpWidget(
+            const MaterialApp(
+              home: MatchInfo(
+                date: "testDate",
+                dayName: "testDayName",
+                time: "testTime",
+                title: title,
+                location: "testLocation",
+                organizer: "testOrganizer",
+                arrivingPlayers: 0,
+              ),
+            ),
+          );
+
+          final titleText = find.text(title);
+
+          expect(titleText, findsOneWidget);
+        },
+      );
+
+      testWidgets(
+        "given location argument is provided"
+        "when widget is rendered"
+        "should show expected location",
+        (widgetTester) async {
+          const location = "testLocation";
+
+          await widgetTester.pumpWidget(
+            const MaterialApp(
+              home: MatchInfo(
+                date: "testDate",
+                dayName: "testDayName",
+                time: "testTime",
+                title: "testTitle",
+                location: location,
+                organizer: "testOrganizer",
+                arrivingPlayers: 0,
+              ),
+            ),
+          );
+
+          final organizerText = find.text(location);
+
+          expect(organizerText, findsOneWidget);
+        },
+      );
+
+      testWidgets(
+        "given organizer argument is provided"
+        "when widget is rendered"
+        "should show expected organizer text",
+        (widgetTester) async {
+          const organizer = "testOrganizer";
+
+          await widgetTester.pumpWidget(
+            const MaterialApp(
+              home: MatchInfo(
+                date: "testDate",
+                dayName: "testDayName",
+                time: "testTime",
+                title: "testTitle",
+                location: "testLocation",
+                organizer: organizer,
+                arrivingPlayers: 0,
+              ),
+            ),
+          );
+
+          final organizerText = find.text("Organized by: $organizer");
+
+          expect(organizerText, findsOneWidget);
+        },
+      );
+
+      testWidgets(
+        "given arrivingPlayers argument is provided"
+        "when widget is rendered"
+        "should show expected arriving players text",
+        (widgetTester) async {
+          const arrivingPlayers = 12;
+
+          await widgetTester.pumpWidget(
+            const MaterialApp(
+              home: MatchInfo(
+                date: "testDate",
+                dayName: "testDayName",
+                time: "testTime",
+                title: "testTitle",
+                location: "testLocation",
+                organizer: "testOrganizer",
+                arrivingPlayers: arrivingPlayers,
+              ),
+            ),
+          );
+
+          final arrivingPlayersText =
+              find.text("Arriving players: $arrivingPlayers");
+
+          expect(arrivingPlayersText, findsOneWidget);
         },
       );
     });
