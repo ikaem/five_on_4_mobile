6e65b3ff44213be36f92e7785c0fba66003f93f8
Karlo M <karlo.marinovic@decode.agency>
2024-01-26 19:34:21 +0100

    stop on non-empty list of participants

--- test/unit/src/features/matches/presentation/widgets/match_create/match_create_participants_data_test.dart
+++ test/unit/src/features/matches/presentation/widgets/match_create/match_create_participants_data_test.dart
@@ -54,6 +54,29 @@ void main() {
                   matching: find.byType(ElevatedButton));
             },
           );
+
+          testWidgets(
+            "given non-emnpty list of participants to invite is provided"
+            "when screen is rendered"
+            "should show expected [PlayerInvitation] widget for each player",
+            (widgetTester) async {
+              // TODO this will probably need to override dependencies later
+              // TODO and will probably need some unified wrapper to push screen on
+              await widgetTester.pumpWidget(
+                const MaterialApp(
+                  home: MatchCreateParticipantsData(
+                    playersToInvite: [
+                      // TODO make this list of playears with some players inside
+                    ],
+                  ),
+                ),
+              );
+
+              // will need again that same thing - scrolling and checking that first and 10th are here
+
+              // will also need to test that invitaation widget - it should accept bool for whether user is currently invited, and callback of onTapInvitationAction - which would invite or cancel invitation depending on the state. and it would accept player of course
+            },
+          );
         },
       );
     },
