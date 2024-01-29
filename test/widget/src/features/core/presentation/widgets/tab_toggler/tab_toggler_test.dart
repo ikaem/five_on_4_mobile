b73de03744e8ca235e9710f9216bbc091df435a6
Karlo M <karlo.marinovic@decode.agency>
2024-01-29 16:50:31 +0100

    pass test show children of tab bar view

--- test/widget/src/features/core/presentation/widgets/tab_toggler/tab_toggler_test.dart
+++ test/widget/src/features/core/presentation/widgets/tab_toggler/tab_toggler_test.dart
@@ -4,6 +4,20 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   const selectorIndicator = "•";
+  final options = [
+    TabTogglerOptionValue(
+      title: "Option 1",
+      child: Container(),
+    ),
+    const TabTogglerOptionValue(
+      title: "Option 2",
+      child: Row(),
+    ),
+    const TabTogglerOptionValue(
+      title: "Option 3",
+      child: Column(),
+    ),
+  ];
 
   group(
     "TabToggler",
@@ -16,21 +30,6 @@ void main() {
             "when widget is rendered"
             "should render a [TabBar] with the same number of tabs as the list of [TabTogglerOptionValue]s",
             (widgetTester) async {
-              final options = [
-                TabTogglerOptionValue(
-                  title: "Option 1",
-                  child: Container(),
-                ),
-                TabTogglerOptionValue(
-                  title: "Option 2",
-                  child: Container(),
-                ),
-                TabTogglerOptionValue(
-                  title: "Option 3",
-                  child: Container(),
-                ),
-              ];
-
               await widgetTester.pumpWidget(
                 MaterialApp(
                   home: Scaffold(
@@ -39,10 +38,38 @@ void main() {
                 ),
               );
 
-              final tabBars =
-                  widgetTester.widgetList(find.byType(Tab)).toList();
+              final tabBarFinder = find.byWidgetPredicate((widget) {
+                if (widget is! TabBar) return false;
+                if (widget.tabs.length != options.length) return false;
 
-              expect(tabBars.length, options.length);
+                return true;
+              });
+
+              expect(tabBarFinder, findsOneWidget);
+            },
+          );
+
+          testWidgets(
+            "given a list of [TabTogglerOptionValue]s"
+            "when widget is rendered"
+            "should render a [TabBarView] with the same number of children as the list of [TabTogglerOptionValue]s",
+            (widgetTester) async {
+              await widgetTester.pumpWidget(
+                MaterialApp(
+                  home: Scaffold(
+                    body: TabToggler(options: options),
+                  ),
+                ),
+              );
+
+              final tabBarViewFinder = find.byWidgetPredicate((widget) {
+                if (widget is! TabBarView) return false;
+                if (widget.children.length != options.length) return false;
+
+                return true;
+              });
+
+              expect(tabBarViewFinder, findsOneWidget);
             },
           );
         },
