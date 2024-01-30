9123c88edb3813c01785a76f1e4a7a23d60aa3f3
Karlo M <karlo.marinovic@decode.agency>
2024-01-30 15:33:49 +0100

    pass home events container renders home events

--- lib/src/features/core/presentation/widgets/home/home_events_container.dart
+++ lib/src/features/core/presentation/widgets/home/home_events_container.dart
@@ -1,3 +1,4 @@
+import 'package:five_on_4_mobile/src/features/core/presentation/widgets/home/home_events.dart';
 import 'package:five_on_4_mobile/src/features/matches/domain/models/match/match_model.dart';
 import 'package:flutter/material.dart';
 
@@ -24,7 +25,7 @@ class HomeEventsContainer extends StatelessWidget {
       );
     }
 
-    return Container();
+    return HomeEvents(matches: matches);
   }
 }
 
