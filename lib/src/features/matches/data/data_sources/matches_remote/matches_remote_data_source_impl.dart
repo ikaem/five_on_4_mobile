f88ef8b24d4e2e7c0de80fde3f1d6e0af85a7b59
Karlo M <karlo.marinovic@decode.agency>
2024-01-31 16:06:05 +0100

    provide args to dio wrapper to get matches

--- lib/src/features/matches/data/data_sources/matches_remote/matches_remote_data_source_impl.dart
+++ lib/src/features/matches/data/data_sources/matches_remote/matches_remote_data_source_impl.dart
@@ -1,5 +1,8 @@
+import 'package:five_on_4_mobile/src/features/core/domain/values/http_request_value.dart';
+import 'package:five_on_4_mobile/src/features/core/utils/constants/http_constants.dart';
 import 'package:five_on_4_mobile/src/features/matches/data/data_sources/matches_remote/matches_remote_data_source.dart';
 import 'package:five_on_4_mobile/src/features/matches/data/entities/match_remote/match_remote_entity.dart';
+import 'package:five_on_4_mobile/src/features/matches/utils/constants/http_matches_constants.dart';
 import 'package:five_on_4_mobile/src/wrappers/libraries/dio/dio_wrapper.dart';
 
 class MatchesRemoteDataSourceImpl implements MatchesRemoteDataSource {
@@ -11,6 +14,20 @@ class MatchesRemoteDataSourceImpl implements MatchesRemoteDataSource {
 
   @override
   Future<List<MatchRemoteEntity>> getMyFollowingMatches() async {
+    final uriParts = HttpRequestUriPartsValue(
+      // TODO use https when we have real server eventually
+      apiUrlScheme: HttpConstants.HTTP_PROTOCOL.value,
+      apiBaseUrl: HttpConstants.BACKEND_BASE_URL_FAKE.value,
+      apiContextPath: HttpConstants.BACKEND_CONTEXT_PATH_FAKE.value,
+      apiEndpointPath:
+          HttpMatchesConstants.BACKEND_ENDPOINT_PATH_MATCHES_FAKE.value,
+      queryParameters: null,
+    );
+
+    final response = await _dioWrapper.get(
+      uriParts: uriParts,
+    );
+
     return [];
   }
 }
