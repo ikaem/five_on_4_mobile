613d6796e15b05f5abefe022d7906a3ce7ccd8a2
Karlo M <karlo.marinovic@decode.agency>
2024-02-02 15:45:28 +0100

    pass store matches locally via data source

--- lib/src/features/matches/data/data_sources/matches_local/matches_local_data_source.dart
+++ lib/src/features/matches/data/data_sources/matches_local/matches_local_data_source.dart
@@ -1,7 +1,7 @@
 import 'package:five_on_4_mobile/src/features/matches/data/entities/match_remote/match_local/match_local_entity.dart';
 
 abstract interface class MatchesLocalDataSource {
-  // Future<List<int>> saveMatches({
-  //   required List<MatchLocalEntity> matches,
-  // });
+  Future<List<int>> saveMatches({
+    required List<MatchLocalEntity> matches,
+  });
 }
