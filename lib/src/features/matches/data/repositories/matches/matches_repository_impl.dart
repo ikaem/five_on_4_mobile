97ef9ef1d99765a03dde3ceb96b5208b65290101
Karlo M <karlo.marinovic@decode.agency>
2024-02-07 21:16:42 +0100

    comments

--- lib/src/features/matches/data/repositories/matches/matches_repository_impl.dart
+++ lib/src/features/matches/data/repositories/matches/matches_repository_impl.dart
@@ -1,6 +1,7 @@
 import 'package:five_on_4_mobile/src/features/auth/data/data_sources/auth_status/auth_status_data_source.dart';
 import 'package:five_on_4_mobile/src/features/matches/data/data_sources/matches_local/matches_local_data_source.dart';
 import 'package:five_on_4_mobile/src/features/matches/data/data_sources/matches_remote/matches_remote_data_source.dart';
+import 'package:five_on_4_mobile/src/features/matches/domain/models/match/match_model.dart';
 import 'package:five_on_4_mobile/src/features/matches/domain/repositories_interfaces.dart';
 import 'package:five_on_4_mobile/src/features/matches/utils/converters/matches_converter.dart';
 
@@ -27,5 +28,21 @@ class MatchesRepositoryImpl implements MatchesRepository {
     );
 
     await _matchesLocalDataSource.saveMatches(matches: matchesLocal);
+  }
+
+  @override
+  Future<List<MatchModel>> getMyMatches() async {
+    final auth = _authStatusDataSource.authDataStatus;
+    if (auth == null) {
+      // TODO log error
+      // TODO also this should logout the user
+      // TODO
+      /* 
+      - we could potentially throw specific exeption here 
+      - the controller should pick it up
+      - then if there is this error - controller should do what? somehow logout? call use case of logout?
+       */
+      throw Exception("User is not authenticated");
+    }
   }
 }
--- lib/src/features/matches/domain/repositories_interfaces.dart
+++ lib/src/features/matches/domain/repositories_interfaces.dart
@@ -1,3 +1,6 @@
+import 'package:five_on_4_mobile/src/features/matches/domain/models/match/match_model.dart';
+
 abstract interface class MatchesRepository {
   Future<void> loadMyMatches();
+  Future<List<MatchModel>> getMyMatches();
 }
