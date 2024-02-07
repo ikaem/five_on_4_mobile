d5f5063390060d45ee7fcfd4fcfa9546da3444b1
Karlo M <karlo.marinovic@decode.agency>
2024-02-07 19:52:41 +0100

    pass test save loaded matches to db

--- lib/src/features/matches/data/repositories/matches/matches_repository_impl.dart
+++ lib/src/features/matches/data/repositories/matches/matches_repository_impl.dart
@@ -1,6 +1,7 @@
 import 'package:five_on_4_mobile/src/features/matches/data/data_sources/matches_local/matches_local_data_source.dart';
 import 'package:five_on_4_mobile/src/features/matches/data/data_sources/matches_remote/matches_remote_data_source.dart';
 import 'package:five_on_4_mobile/src/features/matches/domain/repositories_interfaces.dart';
+import 'package:five_on_4_mobile/src/features/matches/utils/converters/matches_converter.dart';
 
 class MatchesRepositoryImpl implements MatchesRepository {
   const MatchesRepositoryImpl({
@@ -16,5 +17,11 @@ class MatchesRepositoryImpl implements MatchesRepository {
   Future<void> loadMyMatches() async {
     final matchesRemote =
         await _matchesRemoteDataSource.getMyFollowingMatches();
+
+    final matchesLocal = MatchesConverter.fromRemoteEntitiesToLocalEntities(
+      matchesRemote: matchesRemote,
+    );
+
+    await _matchesLocalDataSource.saveMatches(matches: matchesLocal);
   }
 }
