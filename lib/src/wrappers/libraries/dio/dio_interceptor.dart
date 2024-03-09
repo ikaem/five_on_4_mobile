a8e227aed885f784eee282c0952421f43041ae28
Karlo M <karlo.marinovic@decode.agency>
2024-03-09 08:08:02 +0100

    extract redirect logic to a function

--- lib/src/wrappers/libraries/dio/dio_interceptor.dart
+++ lib/src/wrappers/libraries/dio/dio_interceptor.dart
@@ -2,6 +2,7 @@
 // https://dhruvnakum.xyz/networking-in-flutter-interceptors
 
 import 'package:dio/dio.dart';
+import 'package:five_on_4_mobile/src/features/core/utils/constants/http_constants.dart';
 
 // TODO as per https://medium.com/readytowork-org/dio-interceptors-in-flutter-e813f08c2017
 
@@ -33,14 +34,17 @@ class DioInterceptor extends Interceptor {
     // handler.next(options);
     // super.onRequest(options, handler);
     // handler.resolve(response)
-    const localApiPath = "http://10.0.2.2:4000";
+    final requestApiAuthority = options.uri.authority;
+    final shouldRedirectToLocalApi =
+        requestApiAuthority == HttpConstants.BACKEND_BASE_URL.value;
+    if (shouldRedirectToLocalApi) {
+      final localApiOptions = _getRedirectToLocalApiOptions(options: options);
 
-    final localApiOptions = options.copyWith(
-      path: localApiPath + options.uri.path,
-    );
+      handler.next(localApiOptions);
+      return;
+    }
 
-    // handler.next(options);
-    handler.next(localApiOptions);
+    handler.next(options);
   }
 
   @override
@@ -57,4 +61,16 @@ class DioInterceptor extends Interceptor {
     // super.onError(err, handler);
     handler.next(err);
   }
+}
+
+RequestOptions _getRedirectToLocalApiOptions({
+  required RequestOptions options,
+}) {
+  const localApiPath = "http://10.0.2.2:4000";
+
+  final localApiOptions = options.copyWith(
+    path: localApiPath + options.uri.path,
+  );
+
+  return localApiOptions;
 }
