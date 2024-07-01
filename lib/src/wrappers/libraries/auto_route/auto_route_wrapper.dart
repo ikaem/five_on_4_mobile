1e10f8cf902029ac459c8d57ff272b62be88d0f6
Karlo M <karlo.marinovic@decode.agency>
2024-07-01 15:50:28 +0200

    install auto route

---
+++ lib/src/wrappers/libraries/auto_route/auto_route_wrapper.dart
@@ -0,0 +1,9 @@
+import 'package:auto_route/auto_route.dart';
+
+part "auto_route_wrapper.gr.dart";
+
+@AutoRouterConfig()
+class AutoRouteWrapper extends _$AutoRouteWrapper {
+  @override
+  List<AutoRoute> get routes => [];
+}
---
+++ lib/src/wrappers/libraries/auto_route/auto_route_wrapper.gr.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouterGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+part of 'auto_route_wrapper.dart';
+
+abstract class _$AutoRouteWrapper extends RootStackRouter {
+  // ignore: unused_element
+  _$AutoRouteWrapper({super.navigatorKey});
+
+  @override
+  final Map<String, PageFactory> pagesMap = {};
+}
